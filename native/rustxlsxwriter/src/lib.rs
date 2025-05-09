77437496da79d5ccb51c380061b7571f553862d4
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2025-05-09 09:41:12 +0200

    chore: clean up more rust code

--- native/rustxlsxwriter/src/lib.rs
+++ native/rustxlsxwriter/src/lib.rs
@@ -1,14 +1,6 @@
 use rust_xlsxwriter::{Image, Workbook};
-use rustler::Atom;
 use rustler::NifTaggedEnum;
 use std::fmt;
-
-// TODO: do we really need this, we probably want better error messages
-mod atoms {
-    rustler::atoms! {
-        xlsx_generation_error,
-    }
-}
 
 #[derive(NifTaggedEnum)]
 enum CellData {
@@ -30,7 +22,7 @@ impl fmt::Display for CellData {
 }
 
 #[rustler::nif]
-fn write(data: Vec<(u32, u16, CellData)>) -> Result<Vec<u8>, Atom> {
+fn write(data: Vec<(u32, u16, CellData)>) -> Result<Vec<u8>, String> {
     let mut workbook = Workbook::new();
 
     let worksheet = workbook.add_worksheet();
@@ -40,19 +32,19 @@ fn write(data: Vec<(u32, u16, CellData)>) -> Result<Vec<u8>, Atom> {
             CellData::String(val) => worksheet.write_string(row, col, val),
             CellData::Float(val) => worksheet.write_number(row, col, val),
             CellData::ImagePath(val) => match Image::new(val) {
-                Err(_e) => return Err(atoms::xlsx_generation_error()),
+                Err(e) => return Err(e.to_string()),
 
                 Ok(image) => match worksheet.insert_image(row, col, &image) {
                     Ok(val) => Ok(val),
-                    Err(_e) => return Err(atoms::xlsx_generation_error()),
+                    Err(e) => return Err(e.to_string()),
                 },
             },
             CellData::Image(val) => match Image::new_from_buffer(&val) {
-                Err(_e) => return Err(atoms::xlsx_generation_error()),
+                Err(e) => return Err(e.to_string()),
 
                 Ok(image) => match worksheet.insert_image(row, col, &image) {
                     Ok(val) => Ok(val),
-                    Err(_e) => return Err(atoms::xlsx_generation_error()),
+                    Err(e) => return Err(e.to_string()),
                 },
             },
         };
@@ -60,11 +52,11 @@ fn write(data: Vec<(u32, u16, CellData)>) -> Result<Vec<u8>, Atom> {
 
     match workbook.save_to_buffer() {
         Ok(buf) => return Ok(buf),
-        Err(_e) => {
+        Err(e) => {
             // Return an atom saying there was an error.
             // We can figure out later how to include more data
             // about the error.
-            return Err(atoms::xlsx_generation_error());
+            return Err(e.to_string());
         }
     }
 }
