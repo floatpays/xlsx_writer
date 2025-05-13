2b0733dbd51fe716e213b7a62e894f63544fd49f
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2025-05-13 10:56:51 +0200

    chore: update checksums

---
+++ checksum-Elixir.Spreadsheet.Calamine.exs
@@ -0,0 +1,11 @@
+%{
+  "libspreadsheet-v0.1.2-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:7f84d7e0761e3d6fff97e2873f0b6f3a2565ea389442db22f16d3e09a6ca67dc",
+  "libspreadsheet-v0.1.2-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ce983c8d2fd0dc603558a5686d886bcbd0e748e288837d18d428a65b1cb6b8b0",
+  "libspreadsheet-v0.1.2-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:184dfd534c5d400588e4467b479d22478b4d6f9da7d7581e483aab4175da8eb6",
+  "libspreadsheet-v0.1.2-nif-2.16-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:be6f70f1069d2cfa6b01c8d54a82c68f4837c5afe2a84fcd94b2cbf63f6cd15c",
+  "libspreadsheet-v0.1.2-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:2a1af1d4260cf629c85e2e84ac00670a64922c00fa77fa7ff41336ed272930bf",
+  "libspreadsheet-v0.1.2-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:9e23add7e1a59aa2d6481913c590e87cc16b5f70a07f2d53b765d6976caf39eb",
+  "libspreadsheet-v0.1.2-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:d592db9243977aa6dc041ace142404b2b3c099bd1c9ad4f3d6e1158188b13c6e",
+  "spreadsheet-v0.1.2-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:b3cfc9c0eb152a72d130aad07c2c689815f951cdd922f1b5a52a111a5ce955b9",
+  "spreadsheet-v0.1.2-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:0eae3b7b81f2ea1e8e35fcbf707767682d009b7ed20fc3e490c0a4137c1d42fd",
+}
