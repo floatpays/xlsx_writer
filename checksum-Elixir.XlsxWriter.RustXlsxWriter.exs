db93f668e85072eb4db94b384a2bd7c15190742a
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2025-05-15 15:03:13 +0200

    release: update checksums

---
+++ checksum-Elixir.XlsxWriter.RustXlsxWriter.exs
@@ -0,0 +1,11 @@
+%{
+  "libxlsx_writer-v0.1.3-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:c5c28600cce50469a065027a94951670af2b08b8471ac9edc485f48600dc8b58",
+  "libxlsx_writer-v0.1.3-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:2322cfe3204a4609c0ffa03db9a1adedc1146fbe2ce9640382217f41eb0b4c60",
+  "libxlsx_writer-v0.1.3-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:9ee57c6d646012f08059dbda26282237b8c995bc98c1efccf6ffab4958cff32e",
+  "libxlsx_writer-v0.1.3-nif-2.16-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:723b95fd17dd4836d4c88fb70528a50b28bf0b6bba69842a600653429b3597d9",
+  "libxlsx_writer-v0.1.3-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:f62a305d080ffdc3c075958c0b74b02ec03e8372390b89f8efa96c709522a092",
+  "libxlsx_writer-v0.1.3-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f596af3f58e6698baf6100dcb23ffc0351f9cbb48b7828d5d3064d237cb0fee9",
+  "libxlsx_writer-v0.1.3-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:df20a55501cc8de2af5429d15ee43a426544cfef5f7575cf7e425de83ad893db",
+  "xlsx_writer-v0.1.3-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:02d6a50a7c81ae219cc481b5cdd60c35215474ff45813c2c0331d79b81ea42d9",
+  "xlsx_writer-v0.1.3-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:d4dfaf110966e747874e2fd8d85ad6019ad45b5bf2d782cabb211ac866859b2d",
+}
